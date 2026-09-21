3388bb718d2f7db54e6e6e61dc91fd6d2fbf7330
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2026-09-21 15:43:18 +0300

    Create GlobalUserFactory extending UserFactory

---
+++ database/factories/GlobalUserFactory.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Database\Factories\UserFactory;
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends UserFactory {}
