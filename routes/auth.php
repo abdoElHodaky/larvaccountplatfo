17215bd0de8f3eb2bd71c86177638e2922b32a30
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2026-09-22 09:01:30 +0300

    Update email verification routes in auth.php
    Uncomment EmailVerificationPromptController and comment out EmailVerificationNotificationController route.

--- routes/auth.php
+++ routes/auth.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 //use App\Http\Controllers\Auth\ConfirmablePasswordController;
 //use App\Http\Controllers\Auth\EmailVerificationNotificationController;
-//use App\Http\Controllers\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Auth\EmailVerificationPromptController;
 //use App\Http\Controllers\Auth\NewPasswordController;
 //use App\Http\Controllers\Auth\PasswordController;
 //use App\Http\Controllers\Auth\PasswordResetLinkController;
@@ -43,9 +43,9 @@ Route::middleware('auth')->group(function () {
         ->middleware(['signed', 'throttle:6,1'])
         ->name('verification.verify');
 
-    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-        ->middleware('throttle:6,1')
-        ->name('verification.send');
+    //Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+      //  ->middleware('throttle:6,1')
+       // ->name('verification.send');
 
   //  Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
      //   ->name('password.confirm');
