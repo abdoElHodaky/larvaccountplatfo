8a427c4a9bb2c204a9eb0385d9a528195813a503
AbdElrhman ElHodaky <abdo.arh38@yahoo.com>
2026-09-22 10:36:28 +0300

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/js/app.tsx',
+                'resources/js/App.tsx',
             ],
             ssr: 'resources/js/ssr.tsx',
             refresh: true,
